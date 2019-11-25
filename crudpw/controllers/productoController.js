686392d70eb49811a574470f8c5e31da7b1a7988
WalterVRodriguezG <37480815+WalterVRodriguezG@users.noreply.github.com>
2019-11-24 19:12:22 -0600

    Update Controller
    Metodos de Editar y eliminar

--- crudpw/controllers/productoController.js
+++ crudpw/controllers/productoController.js
@@ -34,6 +34,37 @@ router.post('/',(req,res) => {
         if(!err){res.send(doc);}
         else{console.log('Error en el almacenamiento de Productos: ' + JSON.stringify(err, undefined, 2));}
     });
-})
+
+});
+
+router.put('/:id',(req,res)=>{
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`Registro no enontrado con el id: ${req.params.id}`);
+
+    var prod = new Producto({
+        idproducto: req.body.idproducto,
+        nombre: req.body.nombre,
+        presentacion: req.body.presentacion,
+        lote: req.body.lote,
+        preciounit: req.body.preciounit,
+        idclase: req.body.idclase
+    });
+
+    Producto.findByIdAndUpdate(req.params.id,{$set:prod},{new:true},(err,doc) => {
+        if(!err){res.send(doc);}
+        else{console.log('Error en la Actualización del Producto: ' + JSON.stringify(err, undefined, 2));}
+    });
+});
+
+router.delete('/:id',(req,res)=>{
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No hay Registro con ese id: ${req.params.id}`);
+
+    Producto.findByIdAndRemove(req.params.id,{$set:prod},(err,doc) => {
+        if(!err){res.send(doc);}
+        else{console.log('Error al Eliminar el Producto: ' + JSON.stringify(err, undefined, 2));}
+    });
+});
+
 
 module.exports = router;
