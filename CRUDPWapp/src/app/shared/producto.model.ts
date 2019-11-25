13468a1a8fdc6872a468c37e769f2bbdeb6d93b2
WalterVRodriguezG <37480815+WalterVRodriguezG@users.noreply.github.com>
2019-11-24 20:25:34 -0600

    CSS y component Producto
    Agregando Estilos

---
+++ CRUDPWapp/src/app/shared/producto.model.ts
@@ -0,0 +1,10 @@
+export class Producto {
+    _id:string;
+    idproducto: number;
+    nombre: string;
+    presentacion: string;
+    lote: string;
+    preciounit: number;
+    idclase: number;
+}
+
