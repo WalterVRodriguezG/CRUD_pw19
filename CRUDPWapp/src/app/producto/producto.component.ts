ec01fc8428771ac3d859674278b8657ed261b421
WalterVRodriguezG <37480815+WalterVRodriguezG@users.noreply.github.com>
2019-11-25 00:46:16 -0600

    Conexion
    FrontEnd - Backend

--- CRUDPWapp/src/app/producto/producto.component.ts
+++ CRUDPWapp/src/app/producto/producto.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProductoService } from '../shared/producto.service'
+
+declare var M: any;
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
-  styleUrls: ['./producto.component.css']
+  styleUrls: ['./producto.component.css'],
+  providers: [ProductoService]      
 })
 export class ProductoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productoService: ProductoService) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm (form?: NgForm){
+    if(form)
+      form.reset();
+      this.productoService.selectedProducto = {
+        _id:"",
+        idproducto:null,
+        nombre:"",
+        presentacion:"",
+        lote:"",
+        preciounit:null,
+        idclase:null
+      }
+  }
+
+  onSubmit(form: NgForm){
+      this.productoService.postProducto(form.value).subscribe((res) => {
+        this.resetForm(form);
+        M.toast({html: 'Producto grabado exitosamente.', classes: 'rounded'});
+      });
   }
 
 }
--- CRUDPWapp/src/app/shared/producto.service.ts
+++ CRUDPWapp/src/app/shared/producto.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-`import { Observable} from 'rxjs/Observable';`
+`import { Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/toPromise';`
 
 import { Producto } from './producto.model';
 
@@ -10,6 +10,11 @@ import { Producto } from './producto.model';
 export class ProductoService {
   selectedProducto: Producto;
   productos: Producto[];
+  readonly baseURL = 'http://localhost:3000/productos';
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  postProducto(prod : Producto){
+      return this.http.post(this.baseURL, prod);
+  }
 }
